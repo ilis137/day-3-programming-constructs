a3c64f0da0892f7d29b25d90d67bd34bf3be7c26
ilis137 <iliyasshahapure1@gmail.com>
2022-09-14 17:20:52 +0530

    added switch case statement

--- src/main/java/com/javapractice/Employee.java
+++ src/main/java/com/javapractice/Employee.java
@@ -2,46 +2,50 @@ package com.javapractice;
 
 import java.util.Random;
 
+import javax.lang.model.util.ElementScanner6;
+
 /*Calculate Daily Employee Wage*/
 
-public class Employee 
-{
-    int present;
-    int wagePerHour=20;
-    public int isPresent(){
+public class Employee {
+    int wagePerHour = 20;
+    String[] dailyStatus = { "FULL_TIME", "PART_TIME", "NOT_PRESENT" };
+
+    public int dailyWage() {
+        int dailyHrs = 8;
+        return dailyHrs * this.wagePerHour;
+    }
+
+    public int partTimeWage() {
+        int dailyHrs = 4;
+        return dailyHrs * this.wagePerHour;
+    }
+
+    String checkDailyHours() {
+
         Random rand = new Random();
-        return rand.nextInt(2);    
+        return dailyStatus[rand.nextInt(3)];
     }
 
-    public int dailyWage(){
-       int dailyHrs=8;
-       return dailyHrs*this.wagePerHour;
-    }
- 
-    public int partTimeWage(){
-        int dailyHrs=4;
-        return dailyHrs*this.wagePerHour;
-     }
-  
-    public static void main( String[] args )
-    {
-        Employee emp=new Employee();
-        int isPresent=emp.isPresent();
+    public static void main(String[] args) {
+        Employee emp = new Employee();
 
-        if(isPresent==1){
-            System.out.println("Employee is present");
-        }else{
-            System.out.println("Employee is not present");
+        String empCheck = emp.checkDailyHours();
+
+        switch (empCheck) {
+            case "FULL_TIME":
+                int dailyWage = emp.dailyWage();
+                System.out.println("daily wage of employee is: " + dailyWage + " Rs.");
+                break;
+            case "PART_TIME":
+                int partTimeWage = emp.partTimeWage();
+                System.out.println("part time wage of employee is: " + partTimeWage + " Rs.");
+                break;
+            case "NOT_PRESENT":
+                System.out.println(" wage of absent employee is: 0 Rs.");
+                break;
+            default:
+                break;
         }
 
-        
-        
-        int dailyWage=emp.dailyWage();
-        System.out.println("daily wage of employee is: "+dailyWage +" Rs.");
-        
-        
-        int partTimeWage=emp.partTimeWage();
-        System.out.println("part time wage of employee is: "+partTimeWage +" Rs.");
-        
     }
 }
